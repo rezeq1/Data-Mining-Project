761edbadacf613756cb02897fb12057675a27241
rezeq1 <58135379+izaldal@users.noreply.github.com>
2020-07-14 10:37:07 +0300

    fixing

--- Discretization.py
+++ Discretization.py
@@ -27,15 +27,15 @@ class Discritization:
         self.data = data
         self.numberOfbins = numOfbis
 
-    def EqualFrequencyDiscretization(self):
-        array = list(self.data)
+    def EqualFrequencyDiscretization(self, attr):
+        array = list(self.data[attr].to_list())
         array.sort()
         helpList = None
         k = self.numberOfbins
         if type(k) == int:
-            lenB = len(self.data) // k
+            lenB = len(self.data[attr].to_list()) // k
         else:
-            lenB = len(self.data) // len(k)
+            lenB = len(self.data[attr].to_list()) // len(k)
             helpList = k
             k = len(k)
 
@@ -49,7 +49,7 @@ class Discritization:
         if len(array) > 0:
             for x in array:
                 helpm[k - 1].append(x)
-        for x in self.data:
+        for x in self.data[attr].to_list():
             for i in range(len(helpm)):
                 if x in helpm[i]:
                     # newDis.append(f'({min(helpm[i])},{max(helpm[i])})' if not helpList else helpList[i])
@@ -59,32 +59,33 @@ class Discritization:
 
         return newDis
 
-    def Equal_width(self):
+    def Equal_width(self, attr):
         arr = []
         temp = []
         k = self.numberOfbins
-        w = int((max(self.data) - min(self.data)) / k)
-        if (max(self.data) - min(self.data)) / k - w != 0:
+        w = int((max(self.data[attr].to_list()) - min(self.data[attr].to_list())) / k)
+        if (max(self.data[attr].to_list()) - min(self.data[attr].to_list())) / k - w != 0:
             w += 1
 
-        for i in range(0, len(self.data)):
-            if self.data[i] < min(self.data) + w:
-                temp = temp + [self.data[i]]
+        for i in range(0, len(self.data[attr].to_list())):
+            if self.data[attr].to_list()[i] < min(self.data[attr].to_list()) + w:
+                temp = temp + [self.data[attr].to_list()[i]]
         arr.append(temp)
 
         for i in range(1, k - 1):
             temp = []
-            for j in range(0, len(self.data)):
-                if (min(self.data) + w * i) <= self.data[j] < (min(self.data) + w * (i + 1)):
-                    temp = temp + [self.data[j]]
+            for j in range(0, len(self.data[attr].to_list())):
+                if (min(self.data[attr].to_list()) + w * i) <= self.data[attr].to_list()[j] < (
+                        min(self.data[attr].to_list()) + w * (i + 1)):
+                    temp = temp + [self.data[attr].to_list()[j]]
             arr.append(temp)
         temp = []
-        for i in range(0, len(self.data)):
-            if (min(self.data) + w * (k - 1)) <= self.data[i]:
-                temp = temp + [self.data[i]]
+        for i in range(0, len(self.data[attr].to_list())):
+            if (min(self.data[attr].to_list()) + w * (k - 1)) <= self.data[attr].to_list()[i]:
+                temp = temp + [self.data[attr].to_list()[i]]
         arr.append(temp)
         newDis = []
-        for x in self.data:
+        for x in self.data[attr].to_list():
             for i in range(len(arr)):
                 if x in arr[i]:
                     # newDis.append(f'({min(helpm[i])},{max(helpm[i])})' if not helpList else helpList[i])
@@ -93,46 +94,66 @@ class Discritization:
                     break
         return newDis
 
-    def pandas_cut(self):
-        return pd.cut(self.data, self.numberOfbins)
+    def pandas_cut(self, attr):
+        return pd.cut(self.data[attr].to_list(), self.numberOfbins)
 
-    def pandas_qcut(self):
-        return pd.qcut(self.data, self.numberOfbins)
+    def pandas_qcut(self, attr):
+        return pd.qcut(self.data[attr].to_list(), self.numberOfbins)
 
-    def Enropy_Discretization(self):
-        return 0
+    def Enropy_Discretization(self, attr, Class):
+        data = self.data
+        k = len(self.numberOfbins) if type(self.numberOfbins) is list else self.numberOfbins
+        tree = EntropyBased(data, attr, Class, k)
+        rootEnt = tree.entropy
+        bins = tree.getLevel_h()
+        finalBins = []
 
+        bin = 0
+        while bin < k:
+            gain = 0
+            node = None
+            helpBins = list(finalBins)
+            for i in bins:
+                right = i.getRight()
+                left = i.getLeft()
+                helpBins.append(left)
+                helpBins.append(right)
+                for x in bins:
+                    if not (x is i):
+                        helpBins.append(x)
+                infoD = 0
+                for x in helpBins:
+                    infoD += (len(x.data[attr].to_list()) / len(data[attr].to_list())) * x.entropy
+                gainD = rootEnt - infoD
+                if gainD >= gain:
+                    gain = gainD
+                    node = i
+            finalBins.append(node.left)
+            finalBins.append(node.right)
+            bins.remove(node)
+            if len(bins + finalBins) == k:
+                bin = k
+                finalBins = bins + finalBins
+            bin += 1
 
-def Equal_frequency(data, k):
-    arr = []
-    help = data.copy()
-    help.sort()
+        dis = [(min(x.data[attr].to_list()), max(x.data[attr].to_list())) for x in finalBins]
+        dataD = data[attr].to_list()
+        lastBins = []
+        for i in range(len(dataD)):
+            for x in dis:
+                if x[0] <= dataD[i] <= x[1]:
+                    dataD[i] = self.interval(x[0], x[1], self.numberOfbins[dis.index(x)] if type(
+                        self.numberOfbins) is list else None)
+                    break
 
-    size = int(len(data) / k)
-    if len(data) / k - size != 0:
-        size += 1
-
-    for i in range(0, k - 1):
-        temp = []
-        flag = 0
-        for j in range(i * size, (i + 1) * size):
-            if j >= len(data):
-                flag = -1
-            if flag == 0:
-                temp = temp + [help[j]]
-        arr.append(temp)
-    temp = []
-    for i in range((k - 1) * size, len(data)):
-        temp = temp + [help[i]]
-    arr.append(temp)
-    return arr
+        return dataD
 
 
 def EntropyBased(data, attr, Class, k):
     data = data.sort_values(by=attr)
     EntTree = Tree(data)
     split = [bestSplitPoint(data, attr, Class)]
-    bins = [data.loc[data[attr] <= split[0][0]], data.loc[data[attr] > split[0][0]]]
+    # bins = [data.loc[data[attr] <= split[0][0]], data.loc[data[attr] > split[0][0]]]
     finalSplit = []
     depth = log2(k)
 
@@ -150,50 +171,11 @@ def EntropyBased(data, attr, Class, k):
             b.setEntropy(split[1])
             b.setLeft(Tree(data.loc[data[attr] <= split[0]]))
             b.setRight(Tree(data.loc[data[attr] > split[0]]))
-    leafs=EntTree.getLeafs()
+    leafs = EntTree.getLeafs()
     for i in leafs:
         i.setEntropy(Entropy.entropy(i.getRoot()[Class].to_list()))
 
     return EntTree
-
-def Enropy_Discretization(data, attr, Class, k):
-    tree=EntropyBased(data,attr,Class,k)
-    rootEnt=tree.entropy
-    bins=tree.getLevel_h()
-    finalBins=[]
-
-    bin=0
-    while bin<k:
-        gain=0
-        node=None
-        helpBins = list(finalBins)
-        for i in bins:
-            right=i.getRight()
-            left=i.getLeft()
-            helpBins.append(left)
-            helpBins.append(right)
-            for x in bins:
-                if not (x is i):
-                    helpBins.append(x)
-            infoD=0
-            for x in helpBins:
-                infoD+=(len(x.data[attr].to_list())/len(data[attr].to_list()))*x.entropy
-            gainD=rootEnt-infoD
-            if gainD>=gain:
-                gain=gainD
-                node=i
-        finalBins.append(node.left)
-        finalBins.append(node.right)
-        bins.remove(node)
-        if len(bins+finalBins)==k:
-            bin=k
-            finalBins= bins+finalBins
-        bin+=1
-    return tuple([(min(x.data[attr].to_list()),max(x.data[attr].to_list())) for x in finalBins])
-
-
-
-
 
 
 def getMaxGain(list):
@@ -241,5 +223,5 @@ def bestSplitPoint(data, attr, Class, gainD=None):
 data = {"attr": [4, 8, 5, 12, 15, 1, 2, 3, 4, 5], 'class': ['N', 'N', 'Y', 'Y', 'Y'] * 2}
 df = pd.DataFrame(data=data)
 
-x = Enropy_Discretization(df, 'attr', 'class', 4)
-print(x)
+x = Discritization(df, 4)
+print(x.Enropy_Discretization('attr', 'class'))
