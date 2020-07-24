2ba8eeb9780986cc15f1ffb70a96fd54abe2ae28
rezeq1 <rezeqab@ac.sce.ac.il>
2020-07-24 15:40:55 +0300

    add docs to processing file

--- Processing.py
+++ Processing.py
@@ -14,14 +14,34 @@ from sklearn.cluster import KMeans
 from Discretization import Discritization
 
 class PreProcessing():
-
+    '''
+    class that do pre processing
+    '''
     def  Is_Empty(self,df):
+        '''
+        check if data frame is empty if yes return True else False
+        :param df: a data frane
+        :return: nothing
+        '''
         return df.empty
 
     def Delete_Nan_Class_Row(self,df):
+        '''
+        delete the rows that contain class empty value.
+        :param df: a data frane
+        :return: nothing
+        '''
         df.dropna(subset=['class'], inplace=True)
 
     def Fill_Nan_Values(self,df,Structure):
+        '''
+        function that fill the nan values.
+        if the values of the column is  numeric so we fill the average of the values
+        else we fill the most frequent value of the values.
+        :param df: data frame
+        :param Structure: dict that conatin the struct of the data frame
+        :return:nothing
+        '''
         nan_columns = df.columns[df.isna().any()].tolist()
         for col in nan_columns:
             if Structure[col] == 'NUMERIC':
@@ -31,10 +51,24 @@ class PreProcessing():
                 df[col].fillna(Most_Frequent, inplace=True)
 
     def Normalize(self,df,column):
+        '''
+        normalize the values of a given column name
+        :param df: data frame
+        :param column: the name of the column that we want to normalize her values
+        :return:nothing
+        '''
         c = np.array([df[column].tolist()])
         df[column] = (list(preprocessing.normalize(c)[0]))
 
     def Discretization(self,df,Structure,Discretization_type,NumOfBins):
+        '''
+
+        :param df: data frame
+        :param Structure: dict that conatin the struct of the data frame
+        :param Discretization_type: the type of the wanted discretization
+        :param NumOfBins: number of bins for the discretization .
+        :return: nothing
+        '''
         if Discretization_type !='Without Discretization':
             discrize=Discritization(df,NumOfBins)
             Disc={'Equal frequency':discrize.EqualFrequencyDiscretization,
@@ -51,6 +85,11 @@ class PreProcessing():
 
 
     def read_structure(self,FileName):
+        '''
+        the function return a dict that conatin the struct of the data frame by reading the structure file .
+        :param FileName: the file name of the structure file.
+        :return: a dict that conatin the struct of the data frame
+        '''
         struct = pd.read_csv(FileName, sep=' ', names=['type', 'feature', 'data'])
         values = {}
         for i in range(0, struct.shape[0]):
@@ -60,6 +99,14 @@ class PreProcessing():
         return values
 
     def Clean_Data(self,df,Strcture,Discretization_type,NumOfBins):
+        '''
+        take a data frame and do for it binnig and remove the nan class rows and fill the nan values .
+        :param df: data frame
+        :param Structure: dict that conatin the struct of the data frame
+        :param Discretization_type: the type of the wanted discretization
+        :param NumOfBins: number of bins for the discretization .
+        :return: nothing
+        '''
         if self.Is_Empty(df):
             raise Exception("Empty File")
 
@@ -68,12 +115,28 @@ class PreProcessing():
         self.Discretization(df,Strcture,Discretization_type,NumOfBins)
 
     def Save_Data(self,df,Path,FileName):
+        '''
+        save a data frame as a clean file in the give the file.
+        :param df: data frame
+        :param Path: the path of the files
+        :param FileName: the file name of the file that we want to save
+        :return: noting
+        '''
         df.to_csv(Path+'/'+FileName+'_clean.csv')
 
 
 class Processing():
-
+    '''
+    this class for running the algorithm (our implementaion) and for saving the model and building the model.
+    '''
     def Build_Model(self,Path,Algorithm,train):
+        '''
+
+        :param Path: the path of the files
+        :param Algorithm: which algorithm we want to build the model for it
+        :param train: data frame that we want to build the model for it.
+        :return: a model
+        '''
         if Algorithm == 'naive bayes classifier':
             return NaiveBayesClassifier.NaiveBayesClassifier(Path,train)
 
@@ -81,15 +144,33 @@ class Processing():
             return ID3.ID3(Path,train)
 
     def Save_Model(self,Path,model):
+        '''
+        save the given model in the given the path as sav file  by the library pickle
+        :param Path: the path of the files
+        :param model: the model that we want to save
+        :return: nothing
+        '''
         filename = Path+'/model.sav'
         pickle.dump(model, open(filename, 'wb'))
 
     def Load_Model(self,Path):
+        '''
+        load the model that are in the given path and returns it by the library pickle
+        :param Path: the path of the files
+        :return: the model
+        '''
         filename =Path+'/model.sav'
         return pickle.load(open(filename, 'rb'))
 
     def Running_Algorithm(self,Path,Algorithm,train,test):
-
+        '''
+        running the given algorithm for the train and test files and return the results .
+        :param Path: the path of the files
+        :param Algorithm: the wanted algorithm
+        :param train: the train data frame
+        :param test: the test data frame
+        :return: a dict that contain the results for the train and the test files.
+        '''
         if Algorithm == 'naive bayes classifier':
             prediction_train = NaiveBayesClassifier.Testing_model(Path,self.Load_Model(Path),train)
             prediction_test  = NaiveBayesClassifier.Testing_model(Path,self.Load_Model(Path),test)
@@ -121,7 +202,15 @@ class Processing():
 
 
 class BuildAlgorithm():
+    '''
+    this class for running the algorithms (librarys implementaion) and for saving the model and building the model.
+    '''
     def Convert_Strings_To_Numbers(self,Path):
+        '''
+        convert all the strings in the data frame to numbers by a built function and clean the files.
+        :param Path: the path of the files
+        :return: the test and train data frame after running the function on them
+        '''
         pre = PreProcessing()
         try:
             test = pd.read_csv(Path+'/test.csv')
@@ -148,6 +237,15 @@ class BuildAlgorithm():
         return train,test
 
     def Run(self,Algorithm,train,test,Path,n_neighbors=2):
+        '''
+        running the given algorithm for the train and test files and return the results .
+        :param Path: the path of the files
+        :param Algorithm: the wanted algorithm
+        :param train: the train data frame
+        :param test: the test data frame
+        :param n_neighbors: the number of neighbors or clusters for the Knn and Kmeans algorithm.
+        :return: a dict that contain the results for the train and the test files.
+        '''
 
         train_features = train.drop('class', axis=1)
         train_targets = train['class']
