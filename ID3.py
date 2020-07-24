fa943991f81e51812b4f77c1e834148a0f01f983
rezeq1 <rezeqab@ac.sce.ac.il>
2020-07-24 15:45:18 +0300

    add docs to id3 file

--- ID3.py
+++ ID3.py
@@ -4,6 +4,11 @@ from Entropy import InfoGain
 import random
 
 def read_structure(FileName):
+    '''
+    reading the structure file and return a dict that contain the structure of the file
+    :param FileName: the file name of the structure file
+    :return: a dict that contain the structure of the file
+    '''
     struct = pd.read_csv(FileName, sep=' ', names=['type', 'feature', 'data'])
     values = {}
     for i in range(0, struct.shape[0]):
@@ -13,6 +18,12 @@ def read_structure(FileName):
     return values
 
 def Get_Decision_Tree(data, columns):
+    '''
+    build and return the model of the algorithm that we build from the data
+    :param data: data frame that we want to build the model for it
+    :param columns: the columns of the data
+    :return: a model
+    '''
     # Recursion stop conditions
     if len(columns) == 0:
         return np.unique(data['class'])[np.argmax(np.unique(data['class'], return_counts=True)[1])]
@@ -41,7 +52,14 @@ def Get_Decision_Tree(data, columns):
 
 
 def Classification_Row(tree, row, columns,deafult):
-
+    '''
+    return a classification of the row by the given model
+    :param tree: the model
+    :param row: a row that we want to classification
+    :param columns: the columns names
+    :param deafult: a deafult value the we return
+    :return: a classification of the row
+    '''
     root = list(tree.keys())[0]
     result = row[columns.index(root)]
     classification = random.choice(deafult)
@@ -61,7 +79,12 @@ def Classification_Row(tree, row, columns,deafult):
 
 
 def ID3(Path,train):
-
+    '''
+    return and build a model on the given data frame
+    :param Path: the path of the files
+    :param train: the train data frame
+    :return: a model
+    '''
     # get the columns of test file
     columns = train.columns.tolist()
 
@@ -70,6 +93,13 @@ def ID3(Path,train):
     return Get_Decision_Tree(train, columns)
 
 def Testing_model(Path,model,test):
+    '''
+    return the classifications of the rows of the given data frame
+    :param Path: a path that contain the files
+    :param model: the model
+    :param test: the data frame that we want to test the model on it
+    :return:classifications of the rows of the given data frame
+    '''
     # get the rows and the columns of test file
     columns = test.columns.tolist()
     class_attrs = list(set(test['class'].tolist()))
